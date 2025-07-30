f87e40e1c9edc0be92cdc45f3f868982668377ee
rajatbhai980 <raiz41455@gmail.com>
2025-07-30 12:54:53 +0545

    fixed the music

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function secondstominutes(second) {
 
 async function getsongs(folder) {
     curr_folder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`);
+    let a = await fetch(`${folder}/`);
     let response = await a.text();
     let element = document.createElement("div");
     element.innerHTML = response;
@@ -70,7 +70,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs`);
+    let a = await fetch(`songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -84,7 +84,7 @@ async function displayAlbums() {
         if (e.href.includes('/songs')) {
             let folder = e.href.split('/').slice(-2)[0];
             //Get metaData of the folder 
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+            let a = await fetch(`songs/${folder}/info.json`);
             let response = await a.json();
             cardContainer.innerHTML = cardContainer.innerHTML + `
                         <div class="card" data-folder = '${folder}'>
